4a36ca3cb56c7317afe7fa2340c4b3b117bb5e85
Craig <craigtipple81@gmail.com>
2024-07-29 20:08:58 +0100

    Added handle500error handler

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const { getEndpoints } = require("./controllers/api.controller");
-const { handleCustomErrors } = require("./controllers/errors.controller");
+const {
+  handleCustomErrors,
+  handle500errors,
+} = require("./controllers/errors.controller");
 
 const app = express();
 app.use(express.json());
@@ -11,6 +14,8 @@ app.use("/api", getEndpoints);
 
 // Error-handling
 app.use(handleCustomErrors);
+app.use(handle500errors);
+
 app.all("/*", (req, res) => {
   res.status(404).send({ message: "Please check your path is correct." });
 });
--- controllers/errors.controller.js
+++ controllers/errors.controller.js
@@ -1,7 +1,11 @@
 exports.handleCustomErrors = (err, req, res, next) => {
-    if (err.status) {
-      res.status(err.status).send(err.message);
-    } else {
-      next(err);
-    }
-  };
+  if (err.status) {
+    res.status(err.status).send(err.message);
+  } else {
+    next(err);
+  }
+};
+
+exports.handle500errors = (err, req, res, next) => {
+  res.status(500).send({ message: "Internal Server Error" });
+};
