9caa7ed97014137af0ffeb378f3f6cb2b17812c6
Craig <craigtipple81@gmail.com>
2024-08-11 21:21:16 +0100

    Updated Keys

--- config/supabase.js
+++ config/supabase.js
@@ -1,12 +1,12 @@
 // config/supabase.js
-require('dotenv').config();
-const { createClient } = require('@supabase/supabase-js');
+require("dotenv").config();
+const { createClient } = require("@supabase/supabase-js");
 
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
 
 if (!supabaseUrl || !supabaseKey) {
-  throw new Error('Missing Supabase URL or Anon Key');  
+  throw new Error("Missing Supabase URL or Anon Key");
 }
 
 const supabase = createClient(supabaseUrl, supabaseKey);
